2f8d439c910cb2412ca106d3df576054e421dde8
Meimei38 <uyennguyencat@gmail.com>
2022-09-15 15:04:44 +0700

    update

--- sql/ss4_ham_sql/truy_van_qlsv.sql
+++ sql/ss4_ham_sql/truy_van_qlsv.sql
@@ -5,7 +5,7 @@ select * from subject where credit = (select max(credit) from subject);
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
 select * from subject
 join mark on subject.id = mark.subject_id
-and mark = (select max(mark) from mark);
+where mark.mark = (select max(mark) from mark);
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 select student.*, avg (mark) as avarage_mark from student
 join mark on student.id = mark.student_id
