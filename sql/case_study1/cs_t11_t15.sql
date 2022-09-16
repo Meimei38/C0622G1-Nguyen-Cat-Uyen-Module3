9ab175675248d37a615e199440d2b44281bdab56
Meimei38 <uyennguyencat@gmail.com>
2022-09-17 01:55:26 +0700

    update

---
+++ sql/case_study1/cs_t11_t15.sql
@@ -0,0 +1,25 @@
+use co_so_du_lieu_furama;
+-- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+select dvdk.* from dich_vu_di_kem as dvdk 
+join hop_dong_chi_tiet as hdct on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+join hop_dong as hd on hdct.ma_hop_dong = hd.ma_hop_dong
+join khach_hang as kh on kh.ma_khach_hang = hd.ma_khach_hang
+join loai_khach as lk on lk.ma_loai_khach = kh.ma_loai_khach
+where lk.ten_loai_khach = 'Diamond' and (kh.dia_chi like '% Vinh' or kh.dia_chi like '% Quảng Ngãi')
+group by ma_dich_vu_di_kem;
+
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem 
+-- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc 
+-- của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+
+select hd.ma_hop_dong, nv.ho_ten as nhan_vien, kh.ho_ten as khach_hang, kh.so_dien_thoai, dv.ten_dich_vu, dv.ma_dich_vu, ifnull(sum(hdct.so_luong),0) as so_luong_dich_vu_di_kem
+from hop_dong as hd 
+join nhan_vien as nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+join khach_hang as kh on hd.ma_khach_hang = kh.ma_khach_hang
+left join hop_dong_chi_tiet as hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu as dv on dv.ma_dich_vu = hd.ma_dich_vu
+where ((quarter(hd.ngay_lam_hop_dong) = 4) and (year (hd.ngay_lam_hop_dong) = 2020))
+and (dv.ma_dich_vu not in (select dv.ma_dich_vu from dich_vu as dv 
+join hop_dong as hd on dv.ma_dich_vu = hd.ma_dich_vu
+where (quarter(hd.ngay_lam_hop_dong) in (1,2)) and (year (hd.ngay_lam_hop_dong) = 2021)))
+group by hd.ma_hop_dong;
--- sql/case_study1/cs_t1_t10.sql
+++ sql/case_study1/cs_t1_t10.sql
@@ -44,4 +44,51 @@ join hop_dong as hd on dv.ma_dich_vu = hd.ma_dich_vu
 where (quarter(hd.ngay_lam_hop_dong) = 1) 
 group by dv.ma_dich_vu);
 
+-- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
+-- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+select dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu 
+from dich_vu as dv 
+inner join loai_dich_vu as ldv on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+inner join hop_dong as hd on dv.ma_dich_vu = hd.ma_dich_vu
+where (year(hd.ngay_lam_hop_dong)='2020') and dv.ma_dich_vu not in (select dv.ma_dich_vu from dich_vu as dv
+inner join hop_dong as hd on dv.ma_dich_vu = hd.ma_dich_vu
+ where year(hd.ngay_lam_hop_dong)='2021')
+group by dv.ma_dich_vu;
+
+-- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+-- Cách 1:
+select distinct kh.ho_ten from khach_hang as kh;
+
+-- Cách 2:
+select kh.ho_ten from khach_hang as kh
+group by kh.ho_ten;
+
+-- Cách 3: 
+select kh.ho_ten from khach_hang as kh
+union
+select kh.ho_ten from khach_hang as kh;
+
+-- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+select month(hop_dong.ngay_lam_hop_dong) as thang, count(hop_dong.ma_khach_hang) as so_luong_khach from hop_dong 
+where year(hop_dong.ngay_lam_hop_dong) = 2021
+group by (month(hop_dong.ngay_lam_hop_dong)) 
+order by (month(hop_dong.ngay_lam_hop_dong)) asc;
+
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem 
+-- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+select hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, ifnull(sum(hdct.so_luong),0) as so_luong_dich_vu_di_kem
+from hop_dong as hd
+left join hop_dong_chi_tiet as hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+group by hd.ma_hop_dong;
+
+
+
+
+
+
+
 
